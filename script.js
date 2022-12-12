2956df18e030d6bf0fc86207531a37e2be87df94
Joyjeet <joyxbhp6@gmail.com>
2022-12-13 02:07:54 +0530

    Completed

--- script.js
+++ script.js
@@ -20,6 +20,56 @@ let winningPattern = [
 let xTurn = true;
 let count = 0;
 
+// Disable all buttons
+const disableButtons = () => {
+    btnRef.forEach((element) => (element.disabled = true));
+
+    // enable popup
+    popup.classList.remove("hide");
+    
+}
+
+// Enable all buttons (For New Game and Restart)
+const enableButtons = () => {
+    btnRef.forEach((element) => {
+        element.innerText = "";
+        element.disabled = false;
+    });
+    // disable popup
+    popup.classList.add("hide");
+}
+
+
+// New Game
+newgameBtn.addEventListener("click", () => {
+    count = 0;
+    enableButtons();
+})
+
+restartBtn.addEventListener("click", () => {
+    count = 0;
+    enableButtons();
+})
+
+// This function is executed when a player wins
+const winFunction = (letter) => {
+    disableButtons();
+    if(letter == "X")
+    {
+        msgRef.innerHTML = "'X' wins";
+    }
+    else
+    {
+        msgRef.innerHTML ="'O' wins";
+    }
+}
+
+// Function for draw
+const drawFunction = () => {
+    disableButtons();
+    msgRef.innerHTML = "It's a Draw";
+}
+
 // Win Logic
 const winChecker = () => {
     // loop through all win patterns
@@ -29,6 +79,18 @@ const winChecker = () => {
         btnRef[i[1]].innerText,
         btnRef[i[2]].innerText,
     ];
+
+    // Check if elements are filled
+    // If 3 elements are same then give them a win
+
+    if(element1 != "" && (element2 != "") & (element3 != ""))
+    {
+        if(element1 == element2 && element2 == element3)
+        {
+            // if all three buttons have same values then pass the value to win function
+            winFunction(element1);
+        }
+    }
     }
 }
 
@@ -50,13 +112,17 @@ btnRef.forEach((element) => {
 
         // Increment count on each click
         count += 1;
-        if(count === 9)
+        if(count == 9)
         {
             // It's a draw since  there are a total no. of 9 boxes
+            drawFunction();
         }
 
         // Check Win on every click
         winChecker();
     });
 });
+
+// Enable Buttons and disable popup on page load
+window.onload = enableButtons;
 
